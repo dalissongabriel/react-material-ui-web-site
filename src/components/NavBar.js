8810dbc3adca71413c9da77cd52a850ee963f7d9
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2021-10-03 23:40:13 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Box, Container, Link, makeStyles, Toolbar, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      justifyContent: 'space-between',
+    }
+  },
+  siteTitle: {
+    fontWeight: 'bold',
+    letterSpacing: '1.5'
+  },
+  menuBox: {
+    display: 'flex',
+    flexDirection: 'column', 
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row'
+    }
+  },
+  menuOption: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: theme.spacing(10),
+    }
+  },
+}));
+
+export const NavBar = () => {
+
+  const classes = useStyles();
+  
+  return (
+    <Container> 
+      <Toolbar className={classes.toolbar}>
+        <Typography 
+          component='h1'
+          variant='h4'
+          className={classes.siteTitle}>Mammoth Interactive</Typography>
+        <Box className={classes.menuBox}>
+          {['home', 'courses', 'sign-up'].map( menuOptions => (
+              <Link
+                component="button"
+                variant="body1"
+                className={classes.menuOption}>
+                  {menuOptions.toUpperCase()}
+              </Link>
+          ))}
+        </Box>
+      </Toolbar>
+    </Container>
+  )
+}
