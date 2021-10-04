0450e933919eef3c157bcfeb9f7247f84e159888
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2021-10-04 13:15:05 -0300

    added: mocks for products and refactor grid layout

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+
 import { Grid, makeStyles, Typography } from '@material-ui/core'
 import { MainGridItem } from './MainGridItem';
 import { CustomCard } from './CustomCard';
 
-const useStyles = makeStyles((theme)=>({
+import products from '../mocks/products';
+
+
+const useStyles = makeStyles(()=>({
   mainGridItem: {
     backgroundColor: 'blue',
   },
@@ -26,27 +30,18 @@ export const GridLayout = () => {
       <Grid item lg={12} sm={12} xs={12}>
         <MainGridItem />
       </Grid>
-      <Grid item sm={6} xs={12}>
-        <CustomCard 
-          title="Excel Masterclass"
-          subtitle="Recommended for you"
-          description="In this 6-leve eDegree, you'll learn how to use Excel from the very basics all the way to VBA programming and machine learning. You'll also learn web automation, Python and Selenium."
-          image={{
-            url: "https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg",
-            altText: "Excel Masterclass"
-          }}/>
-
-      </Grid>
-      <Grid item sm={6} xs={12}>
-        <CustomCard 
-            title="Excel Masterclass"
-            subtitle="Recommended for you"
-            description="In this 6-leve eDegree, you'll learn how to use Excel from the very basics all the way to VBA programming and machine learning. You'll also learn web automation, Python and Selenium."
+      {products.map(product => (
+        <Grid item sm={6} xs={12}>
+          <CustomCard 
+            title={product.title}
+            subtitle={product.subtitle}
+            description={product.description}
             image={{
-              url: "https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg",
-              altText: "Excel Masterclass"
+              url: product.image.url,
+              altText: product.image.altText
             }}/>
-      </Grid>
+        </Grid>
+      ))}
       <Grid item xs={4} sm={4} >
         <Typography className={classes.secondRowThirdItem}>Second row, third item</Typography>
       </Grid>
---
+++ src/mocks/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    title: "Excel Masterclass",
+    subtitle: "Recommended for you",
+    description: "In this 6-leve eDegree, you'll learn how to use Excel from the very basics all the way to VBA programming and machine learning. You'll also learn web automation, Python and Selenium.'",
+    image: {
+      url: "https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg",
+      altText: "Excel Masterclass",
+    }
+  },
+  {
+    title: "Excel Masterclass",
+    subtitle: "Recommended for you",
+    description: "In this 6-leve eDegree, you'll learn how to use Excel from the very basics all the way to VBA programming and machine learning. You'll also learn web automation, Python and Selenium.'",
+    image: {
+      url: "https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg",
+      altText: "Excel Masterclass",
+    }
+  }
+]
+
+export default products;
